97bed7fbac20fc0a9c77a2b52355fcbb06d704a5
ARNO-KING <eduardojosegonzalezmorales41@gmail.com>
2024-10-23 13:42:59 -0400

    In the proccess of adding the styles

--- src/App.svelte
+++ src/App.svelte
@@ -58,3 +58,29 @@
   </div>
 {/if}
 </main>
+<style>
+
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  .question {
+    margin-bottom: 20em;
+  }
+
+  .options {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+  }
+  </style>
