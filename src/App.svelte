57b1e3f284232a9b40be9915dd3e274cf7dc850a
ARNO-KING <eduardojosegonzalezmorales41@gmail.com>
2024-10-23 13:17:56 -0400

    One series of questions for testing that everything is going well

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,16 @@
 <script>
+  let questions = [
+    {
+      question: "Which command is used to create a new Git repository?",
+      options: ["git init", "git start", "git create", "gitnew"],
+      correctAnswer:0
+    },
+    {
+      question: "Which command is used to add files to the staging area?",
+      options: ["git commit", "git add", "git stage", "git push"],
+      correctAnswer:1
+    },
+  ];
 
 
 
